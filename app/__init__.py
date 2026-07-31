a0d7d8e54b7a05e40551f1870cedc420c26d91fc
Gabriel Soares Segatto <110631466+GabrielSSegatto@users.noreply.github.com>
2026-07-30 22:25:23 -0300

    Update app initialization and database settings
    Refactor app initialization and database configuration.

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,20 @@
 from flask import Flask, render_template
-import os
-from app.service.database import db, migrate
 from flask_login import LoginManager
+from app.service.database import db, migrate
 
-from app.controller.auth_controller import auth_bp  # blueprint auth
+from app.controller.auth_controller import auth_bp
 from app.controller.problem_controller import problem_bp
 from app.controller.test_case_controller import test_case_bp
 from app.controller.submission_controller import submission_bp
-from app.controller.ranking_controller import ranking_bp    
+from app.controller.ranking_controller import ranking_bp
 from app.controller.admin_controller import admin_bp
-from app.controller.contest_controller import contest_bp    
-
+from app.controller.contest_controller import contest_bp
 
 def create_app():
     app = Flask(__name__)
-    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(BASE_DIR, "database.db")
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config['SECRET_KEY'] = 'sua_chave_secreta_aqui'
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SECRET_KEY'] = 'sua-chave-secreta-aqui'
 
     db.init_app(app)
     migrate.init_app(app, db)
@@ -26,9 +23,8 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.init_app(app)
-
-    login_manager.login_view= 'auth.login'
-    login_manager.login_message = "Por favor, faça login para acessar essa página."
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = "Por favor, faça login para acessar esta página."
     login_manager.login_message_category = "info"
 
     from app.models.user import User
@@ -47,5 +43,8 @@ def create_app():
     @app.route("/")
     def home():
         return render_template("home.html")
+
+    with app.app_context():
+        db.create_all()
 
     return app
